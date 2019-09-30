7b5fca2e3d8f859939bb40a7aa232c668630ff20
Oliver <ocben1@gmail.com>
2019-09-30 18:08:07 +1000

    new question functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material';
 import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+
 import {MatCardModule} from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +22,8 @@ import { QuestionComponent } from './question.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/question.component.ts
+++ src/app/question.component.ts
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
 })
 export class QuestionComponent {
 
+    post(question) {
+        console.log(question)
+    }
 }
